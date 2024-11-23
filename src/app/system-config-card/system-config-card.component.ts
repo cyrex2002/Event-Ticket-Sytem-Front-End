615b9b4dd7c146f730be8bbb361b3c139be21dfc
Senuja Jayasekara <senujajayasekara@gmail.com>
2024-11-23 23:00:08 +0530

    added

--- src/app/system-config-card/system-config-card.component.ts
+++ src/app/system-config-card/system-config-card.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
     selector: 'app-system-config-card',
-    imports: [CommonModule, HttpClientModule],
+    imports: [CommonModule, HttpClientModule,BaseChartDirective],
     templateUrl: './system-config-card.component.html',
     styleUrl: './system-config-card.component.css'
 })
@@ -13,6 +15,26 @@ export class SystemConfigCardComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   systemConfig: any = {};
+
+  public pieChartData: ChartData<'pie'> = {
+    labels: ['Total Tickets', 'Free Space'],
+    datasets: [
+      {
+        data: [0, 0],
+        backgroundColor: ['#42A5F5', '#66BB6A'], // Blue and Green colors
+      },
+    ],
+  };
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+    },
+  };
+
   ngOnInit(): void {
     this.fetchSystemConfig();
   }
@@ -21,11 +43,22 @@ export class SystemConfigCardComponent implements OnInit {
     this.http.get<any[]>('http://localhost:8080/get-system-config').subscribe({
       next: (data) => {
         this.systemConfig = data;
+        this.updateChartData();
       },
       error: (error) => {
         console.error('Error fetching system config:', error);
       }
     });
   }
+
+  updateChartData() {
+    const totalTickets = this.systemConfig.totalTickets || 0;
+    const maxCapacity = this.systemConfig.maxTicketCapacity || 0;
+    const freeSpace = maxCapacity - totalTickets;
+
+    this.pieChartData.datasets[0].data = [totalTickets, freeSpace];
+    this.pieChartData = { ...this.pieChartData };
+  }
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerables } from 'chart.js'; // Import everything needed for charts
+import { Chart } from 'chart.js';
 
+Chart.register(...registerables);
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
